a814b865ebfdf2f2068f85f3c1af0db1e51c4a35
SohamKadam <sohamkadam58@gmail.com>
2025-04-26 23:20:33 +0530

    26-04-25

--- temp2/test21.cpp
+++ temp2/test21.cpp
@@ -1,54 +1,75 @@
-// 02 Member Function as Friend
-
+// Function Overriding Example
+// Run Time polymorphism
+// virtual function
+// upcasting
 #include <iostream>
 using namespace std;
 
-// Forward declaration
-class Demo2
+class Base
 {
 public:
-  void fun1();
-  void fun2();
-};
-
-class Demo1
-{
-
-private:
   int no1;
+  int no2;
 
-  friend void Demo2::fun2(); // friend declaration;
+  // virtual function
 
-public:
-  Demo1()
+  // 1000
+  virtual void Display()
   {
-    no1 = 10;
+    cout << "In base class display" << endl;
+  }
+
+  // 2000
+  virtual void Show()
+  {
+    cout << "In Base class show" << endl;
+  }
+
+  // 3000
+  void Hello()
+  {
+    cout << "In Base class Hello" << endl;
   }
 };
 
-// Demo2
-// member function definition
-
-void Demo2::fun1()
+class Derived : public Base
 {
-  cout << "In Demo2 fun1" << endl;
-  Demo1 obj;
-  // cout << "obj no1  = " << obj.no1 << endl; // error
-  cout << "Out Demo2 fun1" << endl;
-}
+public:
+  int no3;
+  int no4;
 
-void Demo2::fun2()
-{
-  cout << "In Demo2 fun2" << endl;
-  Demo1 obj;
-  cout << "obj no1  = " << obj.no1 << endl; // allowed
-  cout << "Out Demo2 fun2" << endl;
-}
+  // 4000
+  void Display()
+  {
+    cout << "In derived class display" << endl;
+  }
+
+  // 5000
+  void Fun()
+  {
+    cout << "In Derived class fun" << endl;
+  }
+
+  // 6000
+  virtual void Greet()
+  {
+    cout << "In Derived class Greet" << endl;
+  }
+};
 
 int main()
 {
-  Demo2 obj2;
-  obj2.fun1();
-  obj2.fun2();
+
+  Derived dObj;
+
+  // Upcasting means base class pointer poiting to derived class object
+  Base *bPtr = &dObj;
+
+  bPtr->Display(); // Derived
+  bPtr->Show();    // Base show
+  bPtr->Hello();   // Base Hello
+  // bPtr->Fun(); // ERROR not present in base class
+  // bPtr->Greet(); // ERROR not present in base class
+
   return 0;
 }
