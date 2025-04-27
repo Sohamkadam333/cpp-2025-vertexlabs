16f371831ed860df672b92d70579110099a81e7c
SohamKadam <sohamkadam58@gmail.com>
2025-04-27 21:03:27 +0530

    27-04-25

--- temp2/test21.cpp
+++ temp2/test21.cpp
@@ -1,75 +1,36 @@
-// Function Overriding Example
-// Run Time polymorphism
-// virtual function
-// upcasting
+
 #include <iostream>
 using namespace std;
 
+// Abstract Class
 class Base
 {
 public:
   int no1;
-  int no2;
 
-  // virtual function
-
-  // 1000
-  virtual void Display()
-  {
-    cout << "In base class display" << endl;
-  }
-
-  // 2000
-  virtual void Show()
-  {
-    cout << "In Base class show" << endl;
-  }
-
-  // 3000
-  void Hello()
-  {
-    cout << "In Base class Hello" << endl;
-  }
+  virtual void Display() = 0; // pure virtual function
 };
 
 class Derived : public Base
 {
 public:
-  int no3;
-  int no4;
+  int no2;
 
-  // 4000
+  void Show()
+  {
+    cout << "Hello From Derived Show" << endl;
+  }
+
   void Display()
   {
-    cout << "In derived class display" << endl;
-  }
-
-  // 5000
-  void Fun()
-  {
-    cout << "In Derived class fun" << endl;
-  }
-
-  // 6000
-  virtual void Greet()
-  {
-    cout << "In Derived class Greet" << endl;
+    cout << "In Derived Display" << endl;
   }
 };
 
-int main()
+int main(void)
 {
-
   Derived dObj;
-
-  // Upcasting means base class pointer poiting to derived class object
-  Base *bPtr = &dObj;
-
-  bPtr->Display(); // Derived
-  bPtr->Show();    // Base show
-  bPtr->Hello();   // Base Hello
-  // bPtr->Fun(); // ERROR not present in base class
-  // bPtr->Greet(); // ERROR not present in base class
+  dObj.Display();
 
   return 0;
 }
